dcc76b5b775ec7b02b704d9d77161a923ab04abe
xenon830 <xenon830@hotmail.com>
2018-06-16 03:12:50 +0800

    Completed Section 9 Lecture 140

--- Blog/src/components/posts_new.js
+++ Blog/src/components/posts_new.js
@@ -1,13 +1,73 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
-    render(){
-        return (
-            <div>
-                PostsNew!
+    renderField(field){
+        const {meta:{touched, error}} = field;
+        const className=`form-group ${touched && error ? 'has-danger':''}`;
+
+        return(
+            <div className={className}>
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                <div className='text-help'>
+                    {touched ? error:''}
+                </div>
             </div>
+        );
+    }
+
+    onSubmit(values){
+        console.log(values);
+    }
+
+    render(){
+        const {handleSubmit} = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         );
     }
 }
 
-export default PostsNew
+function validate(values){
+    const error = {};
+
+    if(!values.title) {
+        error.title = "Enter a title!";
+    }
+    if(!values.categories) {
+        error.categories = "Enter some categories!";
+    }
+    if(!values.content) {
+        error.content = "Enter some content!";
+    }
+
+    return error;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
