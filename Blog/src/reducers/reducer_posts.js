e76e75328efec5bf127baf814fcdb68772f4f142
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-06-19 22:42:44 +0800

    clean up missing code since last commit

--- Blog/src/reducers/reducer_posts.js
+++ Blog/src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST, CREATE_POST} from '../actions';
 import _ from 'lodash';
 
 export default function(state={}, action){
@@ -6,6 +6,9 @@ export default function(state={}, action){
 
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+
+        case FETCH_POST:
+            return {...state, [action.payload.data.id]: action.payload.data};
 
         default:
             return state;
